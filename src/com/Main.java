48c7f861e9c35947bc8ec0c0bfb3babc045846b8
Gamester1128 <77338063+Gamester1128@users.noreply.github.com>
2021-11-18 02:33:08 +0000

    Adding better info output and program can be closed by pressing enter

--- src/com/Main.java
+++ src/com/Main.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -15,8 +16,8 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Main {
-	
-	// path to config file 
+
+	// path to config file
 	private static final String configPath = "config.json";
 
 	public static void main(String[] args) {
@@ -33,30 +34,56 @@ public class Main {
 		char cfg_unit = ((String) json.get("unit")).charAt(0);
 		int cfg_time = (int) (long) json.get("time");
 
-		// Initialise
+		// Print configuration
+		printInfo("Soundtrack path = \"" + cfg_url + "\"");
+		printInfo("Unit = " + cfg_unit);
+		printInfo("Raw Time (no units) = " + cfg_unit);
+
 		Random rand = new Random();
 		boolean running = true;
 		int i = 1;
+
+		// For another thread, will close program after any input
+		Runnable closeAfterInput = () -> {
+			Scanner scanner = new Scanner(System.in);
+			scanner.nextLine();
+			printInfo("John Cena is going to rest for now...");
+			scanner.close();
+			System.exit(0);
+		};
+
+		// Thread responsible for closing program when anything is input
+		new Thread(closeAfterInput, "Waits for User Input").start();
+
+		// get multiplier for how many milllis to sleep based on cfg_unit
+		int mulitiplier = 1;
+		switch (cfg_unit) {
+		case 'm':
+			mulitiplier *= 60;
+		case 's':
+			mulitiplier *= 1000;
+		}
+
+		// stores the upper bound for random
+		int upperTime = (int) cfg_time * mulitiplier;
+
+		printInfo("Always Lower Bound for Random: 0ms");
+		printInfo("Always Upper Bound for Random: " + (upperTime - 1) + "ms");
+
+		// not sure if you know but its a while loop, a for loop without
+		// initialisation of variable and doing something to a variable at end
+		// of iteration
 		while (running) {
-
-			// get multiplier for how many milllis to sleep based on cfg_unit
-			int mulitiplier = 1;
-			switch (cfg_unit) {
-			case 'm':
-				mulitiplier *= 60;
-			case 's':
-				mulitiplier *= 1000;
-			}
-			// stores the upper bound for random
-			int randTime = (int) cfg_time * mulitiplier;
-			System.out.println("Lower Bound for Random: 0ms");
-			System.out.println("Upper Bound for Random: " + (randTime - 1) + "ms");
-
-			sleep(rand.nextInt(randTime));
-
-			System.out.println("#" + i + " : Playing Sound!");
+			int randTime = rand.nextInt(upperTime);
+			printInfo("Waiting : " + randTime + "ms");
+			sleep(randTime);
+			printInfo("#" + i++ + " : Playing sound, becareful pepeLaugh");
 			playSound(cfg_url);
 		}
+	}
+
+	public static void printInfo(String s) {
+		System.out.println("[BINCHILLIN/INFO] : " + s);
 	}
 
 	// puts main thread to sleep
